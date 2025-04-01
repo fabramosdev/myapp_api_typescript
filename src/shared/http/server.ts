2e7f457759e68e24f00b787d1cbcdec36fe5b33b
Fabiano Santos Ramos <fabianosantosramos1978@gmail.com>
2025-04-01 12:07:17 -0300

    config routes

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,15 +1,14 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import { routes } from './routes';
 
 const app = express();
 app.use(cors());
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Olá Fabiano' });
-});
+app.use(routes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started on port ${process.env.PORT}`);
