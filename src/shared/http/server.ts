a3748444592bc8cf58c512b6a16c40c9d770f9db
Fabiano Santos Ramos <fabianosantosramos1978@gmail.com>
2025-04-01 11:27:50 -0300

    add cors, libs and express

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,15 @@
-export class AppServer {
-  private app: string;
+import express from 'express';
+import cors from 'cors';
 
-  constructor(info: string) {
-    this.app = info ?? 'Ola Dev';
-  }
-}
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Olá Fabiano' });
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
