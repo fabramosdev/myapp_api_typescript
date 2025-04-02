2cb81a56287c90b520e7a53e2ad0e32c23bd38e4
Fabiano Santos Ramos <fabianosantosramos1978@gmail.com>
2025-04-02 16:47:28 -0300

    create migration CreateRolesTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,8 +1,9 @@
 import { DataSource } from 'typeorm';
+import { CreateRolesTable1743622486013 } from './migrations/1743622486013-CreateRolesTable';
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [],
-  migrations: [],
+  migrations: [CreateRolesTable1743622486013],
 });
