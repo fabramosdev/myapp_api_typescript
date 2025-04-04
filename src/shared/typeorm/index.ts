a4c9d45c7a84ee6fe38d9e674f7cf14604e28bca
Fabiano Santos Ramos <fabianosantosramos1978@gmail.com>
2025-04-04 08:39:37 -0300

    chore: create migration CreateUsersTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,10 +1,10 @@
 import { DataSource } from 'typeorm';
 import { Role } from '@roles/entities/Role';
-import { CreateRolesTable1743622486013 } from './migrations';
+import { CreateRolesTable1743622486013, CreateUsersTable1743766327769 } from './migrations';
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [Role],
-  migrations: [CreateRolesTable1743622486013],
+  migrations: [CreateRolesTable1743622486013, CreateUsersTable1743766327769],
 });
--- src/shared/typeorm/migrations/index.ts
+++ src/shared/typeorm/migrations/index.ts
@@ -1,4 +1,4 @@
 // Migrations by Chronologic Creation
 import { CreateRolesTable1743622486013 } from './1743622486013-CreateRolesTable';
-
-export { CreateRolesTable1743622486013 };
+import { CreateUsersTable1743766327769 } from './1743766327769-CreateUsersTable';
+export { CreateRolesTable1743622486013, CreateUsersTable1743766327769 };
