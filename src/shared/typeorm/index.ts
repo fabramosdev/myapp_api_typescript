3beb4588a21c2512a2c8ce2e70996e1251ac71e8
Fabiano Santos Ramos <fabianosantosramos1978@gmail.com>
2025-04-04 08:55:07 -0300

    chore: create relations on Users & Roles Table

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,10 +1,18 @@
 import { DataSource } from 'typeorm';
 import { Role } from '@roles/entities/Role';
-import { CreateRolesTable1743622486013, CreateUsersTable1743766327769 } from './migrations';
+import {
+  CreateRolesTable1743622486013,
+  CreateUsersTable1743766327769,
+  AddRoleIdToUsersTable1743766862153,
+} from './migrations';
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [Role],
-  migrations: [CreateRolesTable1743622486013, CreateUsersTable1743766327769],
+  migrations: [
+    CreateRolesTable1743622486013,
+    CreateUsersTable1743766327769,
+    AddRoleIdToUsersTable1743766862153,
+  ],
 });
--- src/shared/typeorm/migrations/index.ts
+++ src/shared/typeorm/migrations/index.ts
@@ -1,4 +1,12 @@
 // Migrations by Chronologic Creation
 import { CreateRolesTable1743622486013 } from './1743622486013-CreateRolesTable';
 import { CreateUsersTable1743766327769 } from './1743766327769-CreateUsersTable';
-export { CreateRolesTable1743622486013, CreateUsersTable1743766327769 };
+import { AddRoleIdToUsersTable1743766862153 } from './1743766862153-AddRoleIdToUsersTable';
+/**
+ * Centralização de migrations
+ */
+export {
+  CreateRolesTable1743622486013,
+  CreateUsersTable1743766327769,
+  AddRoleIdToUsersTable1743766862153,
+};
