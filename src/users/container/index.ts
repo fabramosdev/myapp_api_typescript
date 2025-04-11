f9ced367b1ade51da86521ac8c0be80eaac28057
Fabiano Santos Ramos <fabianosantosramos1978@gmail.com>
2025-04-11 08:53:02 -0300

    add login controller

--- src/users/container/index.ts
+++ src/users/container/index.ts
@@ -1,5 +1,6 @@
 import { IUsersRepository } from '@users/repositories/IUsersRepository';
 import { UsersRepository } from '@users/repositories/UsersRepository';
+import { CreateLoginController } from '@users/useCases/createLogin/CreateLoginController';
 import { CreateUserController } from '@users/useCases/createUser/CreateUserController';
 import { ListUsersController } from '@users/useCases/listUSers/ListUsersController';
 import { container } from 'tsyringe';
@@ -7,3 +8,4 @@ import { container } from 'tsyringe';
 container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository);
 container.registerSingleton('CreateUserController', CreateUserController);
 container.registerSingleton('ListUsersController', ListUsersController);
+container.registerSingleton('CreateLoginController', CreateLoginController);
--- src/users/http/users.routes.ts
+++ src/users/http/users.routes.ts
@@ -3,11 +3,13 @@ import { container } from 'tsyringe';
 import { CreateUserController } from '@users/useCases/createUser/CreateUserController';
 import { createUserValidator, listUsersValidator } from '@users/validators';
 import { ListUsersController } from '@users/useCases/listUSers/ListUsersController';
+import { CreateLoginController } from '@users/useCases/createLogin/CreateLoginController';
 
 const usersRouter = Router();
 
 const createUserController = container.resolve(CreateUserController);
 const listUsersController = container.resolve(ListUsersController);
+const createLoginController = container.resolve(CreateLoginController);
 
 usersRouter.post('/', createUserValidator, (request, response): any => {
   return createUserController.handle(request, response);
@@ -15,6 +17,10 @@ usersRouter.post('/', createUserValidator, (request, response): any => {
 
 usersRouter.get('/', listUsersValidator, (request, response): any => {
   return listUsersController.handle(request, response);
+});
+
+usersRouter.post('/login', (request, response): any => {
+  return createLoginController.handle(request, response);
 });
 
 export { usersRouter };
