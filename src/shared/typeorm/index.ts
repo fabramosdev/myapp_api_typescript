831423841f7916e5c208bac8be7afb222b52a2b0
Fabiano Santos Ramos <fabianosantosramos1978@gmail.com>
2025-04-18 19:20:57 -0300

    fix: migrations path

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,13 +1,10 @@
 import { DataSource } from 'typeorm';
 import { Role } from '@roles/entities/Role';
 import { User } from '@users/entities/User';
-
-import {
-  CreateRolesTable1743622486013,
-  CreateUsersTable1743766327769,
-  AddRoleIdToUsersTable1743766862153,
-  CreateRefreshTokenTable1745013916536,
-} from './migrations';
+import { CreateRolesTable1743622486013 } from './migrations/1743622486013-CreateRolesTable';
+import { CreateUsersTable1743766327769 } from './migrations/1743766327769-CreateUsersTable';
+import { AddRoleIdToUsersTable1743766862153 } from './migrations/1743766862153-AddRoleIdToUsersTable';
+import { CreateRefreshTokenTable1745013916536 } from './migrations/1745013916536-CreateRefreshTokenTable';
 
 export const dataSource = new DataSource({
   type: 'sqlite',
